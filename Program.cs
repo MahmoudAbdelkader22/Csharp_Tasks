fa63f19f21f31d819fc293f2e5be870fa609770a
Mahmoud Abd Elkader Mahmoud <tito17383@gmail.com>
2026-08-13 06:36:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Csharp_day01
+{
+     class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
